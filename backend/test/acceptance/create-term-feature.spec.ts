1471ed65fcb68d1512b35d2c1f85372d996baec7
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2024-06-23 23:58:20 +0200

    feat: User should be able to create a Term and save it to the database

--- backend/test/acceptance/create-term-feature.spec.ts
+++ backend/test/acceptance/create-term-feature.spec.ts
@@ -1,21 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateTermController } from '../../src/terms/infrastructure/http/create-term.controller';
 import { CreateTermDto } from '../../src/terms/infrastructure/http/dtos/create-term.dto';
-import { CqrsModule } from '@nestjs/cqrs';
-import { TermsKeys } from '../../src/terms/infrastructure/terms.keys';
-import { InMemoryTermsRepository } from '../../src/terms/infrastructure/persistence/inMemoryTermsRepository';
-import { RandomUuidService } from '../../src/shared/infrastructure/randomUuidService';
-import { NestCreateTermCommandHandler } from '../../src/terms/infrastructure/handlers/nest-create-term-command.handler';
-import { AppModule } from '../../src/app.module';
 import { TermsModule } from '../../src/terms/infrastructure/terms.module';
+import { TermsKeys } from '../../src/terms/infrastructure/terms.keys';
+import { TermsRepository } from '../../src/terms/domain/terms.repository';
+import { UuidService } from '../../src/terms/application/uuid.service';
+import { Term } from '../../src/terms/domain/term';
 
 describe('Create Term Feature', () => {
     let controller: CreateTermController;
+    let repository: TermsRepository;
+    let uuidService: UuidService;
 
     beforeEach(async () => {
+        repository = {
+            save: jest.fn(),
+        };
+        uuidService = {
+            newUuid: jest.fn(),
+        };
         const module: TestingModule = await Test.createTestingModule({
             imports: [TermsModule],
-        }).compile();
+        })
+            .overrideProvider(TermsKeys.UUID_SERVICE)
+            .useValue(uuidService)
+            .overrideProvider(TermsKeys.TERMS_REPOSITORY)
+            .useValue(repository)
+            .compile();
 
         controller = module.get<CreateTermController>(CreateTermController);
 
@@ -23,11 +34,24 @@ describe('Create Term Feature', () => {
         await module.init();
     });
 
-    test('User should be able to create a Term', async () => {
+    it('User should be able to create a Term and save it to the database', async () => {
         const term = 'Git';
         const description = 'A version control system';
         const createTermDto = new CreateTermDto(term, description);
+        const id = '3579c66e-91a1-4170-ad01-7186e4184f2b';
+        uuidService.newUuid = jest.fn().mockReturnValue(id);
 
         await controller.createTerm(createTermDto);
+
+        expect(repository.save).toHaveBeenCalledTimes(1);
+        expect(repository.save).toHaveBeenCalledWith(
+            new Term({
+                id,
+                props: {
+                    term,
+                    description,
+                },
+            }),
+        );
     });
 });
