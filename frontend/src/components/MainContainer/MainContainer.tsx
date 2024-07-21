241c6c9d5813e74fbb6751cd4a794c0d897cec8b
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2024-07-22 00:40:17 +0200

    fix: remove MainContainer styled

--- frontend/src/components/MainContainer/MainContainer.tsx
+++ frontend/src/components/MainContainer/MainContainer.tsx
@@ -1,9 +1,4 @@
-import { styled } from 'styled-components';
+import {PropsWithChildren} from "react";
 
-export const MainContainer = styled.div`
-  padding: 1rem;
-  margin: 0 auto;
-  max-width: 1200px;
-  min-height: 100vh;
-  position: relative;
-`;
+export const MainContainer = ({children}: PropsWithChildren) => <div
+  className="min-h-screen p-6 max-w-7xl my-0 mx-auto relative">{children}</div>
